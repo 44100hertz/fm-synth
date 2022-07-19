6b86ab5fb8387b8f1b5845169d2fc658d03e3934
Sam Pagenkopf <ssaammp@gmail.com>
2022-07-18 21:24:53 -0500

    move sound gen worker to src/lib

--- src/lib/SoundGen.svelte
+++ src/lib/SoundGen.svelte
@@ -1,36 +1,37 @@
 <script lang="ts">
-	import Slider from "$lib/Slider.svelte"
-	import Knob from "$lib/Knob.svelte"
+ import Slider from "$lib/Slider.svelte"
+ import Knob from "$lib/Knob.svelte"
+ import SoundGenWorker from "$lib/SoundGenWorker.js?url"
 
-    import { onMount, onDestroy } from 'svelte'
-    let stop = false;
-    let pitch = 2200;
-    let pitchRatio = 0.5;
-    let depth = 1;
+ import { onMount, onDestroy } from 'svelte'
+ let stop = false;
+ let pitch = 2200;
+ let pitchRatio = 0.5;
+ let depth = 1;
 
-    onMount(async () => {
-        const ac = new AudioContext();
-        await ac.audioWorklet.addModule('soundgen.js', { type: 'module' });
-        const waveNode = new AudioWorkletNode(ac, 'wave-generator');
-        waveNode.connect(ac.destination);
-        const p = waveNode.port;
-        p.postMessage(['srate', ac.sampleRate]);
+ onMount(async () => {
+     const ac = new AudioContext();
+     await ac.audioWorklet.addModule(SoundGenWorker, { type: 'module' });
+     const waveNode = new AudioWorkletNode(ac, 'wave-generator');
+     waveNode.connect(ac.destination);
+     const p = waveNode.port;
+     p.postMessage(['srate', ac.sampleRate]);
 
-        let iv = window.setInterval(() => {
-//            p.postMessage(['freq', 400]);
-            p.postMessage(['freq', pitch]);
-            p.postMessage(['modDepth', depth]);
-            p.postMessage(['carPitchRatio', pitchRatio]);
-            if (stop) {
-                p.postMessage('stop');
-                window.clearInterval(iv);
-            }
-        }, 16);
-    });
+     let iv = window.setInterval(() => {
+         //            p.postMessage(['freq', 400]);
+         p.postMessage(['freq', pitch]);
+         p.postMessage(['modDepth', depth]);
+         p.postMessage(['carPitchRatio', pitchRatio]);
+         if (stop) {
+             p.postMessage('stop');
+             window.clearInterval(iv);
+         }
+     }, 16);
+ });
 
-    onDestroy(() => {
-        stop = true;
-    });
+ onDestroy(() => {
+     stop = true;
+ });
 </script>
 
 <!--<Slider bind:value="{pitch}" label="Pitch" min="{20}" max="{8000}" log=true/>-->
